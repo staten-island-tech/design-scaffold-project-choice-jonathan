0807b764e8af5fc758f76f7c739d58f6e70cd263
Daniel Tiourine <90714623+DanielsPotatos@users.noreply.github.com>
2022-02-28 13:55:36 -0500

    sssss

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig){
-    eleventyConfig.addPassthroughCopy("src/styles");
+    eleventyConfig.addPassthroughCopy("src/style");
     eleventyConfig.addPassthroughCopy("src/md");
     eleventyConfig.addPassthroughCopy("src/media");
     return{
