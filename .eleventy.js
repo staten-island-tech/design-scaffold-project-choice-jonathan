690ee0e196b3fd75e951166044a047cfb69c0ead
Daniel Tiourine <90714623+DanielsPotatos@users.noreply.github.com>
2022-03-07 13:58:41 -0500

    ss

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
+
+const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("src/style");
     eleventyConfig.addPassthroughCopy("src/md");
     eleventyConfig.addPassthroughCopy("src/media");
+    eleventyConfig.addFilter()
     return{
         dir:{
             input:"src",
